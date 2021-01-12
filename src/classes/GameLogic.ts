a0a604e6f80762315231de763eb8a5730643efc0
Sjimmie <sjimmie.bleeker@student.nhlstenden.com>
2021-01-12 20:15:29 +0100

    GameLogic (#26)

---
+++ src/classes/GameLogic.ts
@@ -0,0 +1,14 @@
+// TODO: remove eslint-disable when class is implemented
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Direction } from '@/models/Direction'
+
+export class GameLogic {
+  public Initialize (): void {
+
+  }
+
+  public ChangeDirection (direction: Direction): void {
+
+  }
+}
