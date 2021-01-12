98db5a524d83014e7579482b04a1a22d02f05e12
Sjimmie <sjimmie.bleeker@student.nhlstenden.com>
2021-01-12 19:11:47 +0100

    GameLogic

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { GameLogic } from './classes/GameLogic'
 import store from './store'
 
 const app = createApp(App)
 app.use(store)
 app.mount('#app')
+
+const gameLogic = new GameLogic()
+gameLogic.Initialize()
