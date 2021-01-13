79c8df8a1363be7dd708e1f54caf357017ad0750
Sjimmie <sjimmie.bleeker@student.nhlstenden.com>
2021-01-13 22:09:43 +0100

    Added direction

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,22 +1,31 @@
+import { Direction } from '@/models/Direction'
 import { TileState } from '@/models/TileState'
 import { createStore } from 'vuex'
 
 export interface State {
   board: Array<Array<TileState>>;
+  direction: Direction;
 }
 
 export const store = createStore<State>({
   state: {
-    board: []
+    board: [],
+    direction: Direction.Right
   },
   mutations: {
     updateBoard (state, board: Array<Array<TileState>>) {
       state.board = board
+    },
+    updateDirection (state, direction: Direction) {
+      state.direction = direction
     }
   },
   getters: {
     board: state => {
       return state.board
+    },
+    direction: state => {
+      return state.direction
     }
   }
 })
